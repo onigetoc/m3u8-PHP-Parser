f89c23b6f546ce7398aa0da3f4fa041a11e07c82
Gino <ginocote@gmail.com>
2017-09-12 17:27:35 -0400

    Update Simple more simple and with ID

--- m3u-parser-simple.php
+++ m3u-parser-simple.php
@@ -30,7 +30,7 @@ $url = $_GET["url"];
 if(isset($url)) {
   $m3ufile = file_get_contents($url);
 } else {
-  $m3ufile = file_get_contents('https://pastebin.com/raw/t1mBJ2Yi');
+  $m3ufile = file_get_contents('http://pastebin.com/raw/QtUHJ78r');
 }
 
 //$re = '/#(EXTINF|EXTM3U):(.+?)[,]\s?(.+?)[\r\n]+?((?:https?|rtmp):\/\/(?:\S*?\.\S*?)(?:[\s)\[\]{};"\'<]|\.\s|$))/';
@@ -39,28 +39,53 @@ $re = '/#EXTINF:(.+?)[,]\s?(.+?)[\r\n]+?((?:https?|rtmp):\/\/(?:\S*?\.\S*?)(?:[\
 $attributes = '/([a-zA-Z0-9\-\_]+?)="([^"]*)"/';
 
 
-$m3ufile = str_replace('tvg-logo', 'thumb_square', $m3ufile);
-$m3ufile = str_replace('tvg-id', 'id', $m3ufile);
-$m3ufile = str_replace('tvg-name', 'author', $m3ufile);
-$m3ufile = str_replace('group-title', 'group', $m3ufile);
+//$m3ufile = str_replace('tvg-logo', 'thumb_square', $m3ufile);
+//$m3ufile = str_replace('tvg-id', 'id', $m3ufile);
+////$m3ufile = str_replace('tvg-name', 'group', $m3ufile);
+////$m3ufile = str_replace('tvg-name', 'name', $m3ufile);
+//$m3ufile = str_replace('tvg-name', 'author', $m3ufile);
+//$m3ufile = str_replace('group-title', 'group', $m3ufile);
+
+// WAY TO REPLACE ALL -
+//$m3ufile = str_replace('-', '', $m3ufile);
+
+//print_r($m3ufile);
+
+//$m3ufile = str_replace(' ', '_', $m3ufile); // FOR GROUP
 
 preg_match_all($re, $m3ufile, $matches);
 
+// Print the entire match result
+//print_r($matches);
+
 $items = array();
 
+$i = 1;
+
  foreach($matches[0] as $list) {
+    
+     //echo "$list <br>";
 	 
    preg_match($re, $list, $matchList);
 
+   //$mediaURL = str_replace("\r\n","",$matchList[4]);
+   //$mediaURL = str_replace("\n","",$matchList[4]);
+   //$mediaURL = str_replace("\n","",$mediaURL);
    $mediaURL = preg_replace("/[\n\r]/","",$matchList[3]);
    $mediaURL = preg_replace('/\s+/', '', $mediaURL);
+   //$mediaURL = preg_replace( "/\r|\n/", "", $matches[4] );
+   
 
    $newdata =  array (
+    //'ATTRIBUTE' => $matchList[2],
+    'id' => $i++,
     'service' => "iptv",
     'title' => $matchList[2],
+    //'playlistURL' => (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",
     'playlistURL' => $_SERVER['QUERY_STRING'],
     'playlistURL' => str_replace("url=","",$_SERVER['QUERY_STRING']),
     'media_url' => $mediaURL,
+//    'author' => "Author name or provider",
     'url' => $mediaURL
     );
     
@@ -68,12 +93,28 @@ $items = array();
     
     foreach ($matches as $match) {
        $newdata[$match[1]] = $match[2];
-    } 
-	$items[] = $newdata;
+    }
+    
+    //array_push($newdata,$attribute);
+    //$newdata[] = $attribute;
+	 
+	 $items[] = $newdata;
+	 //$items[] = $matchList[2];
     
  }
 
+//   $globalitem =  array (
+//    //'ATTRIBUTE' => $matchList[2],
+//    'item' => $items
+//    );
+
+//$globalitem[$items] ;
+//$globalitems['item'] = $items;
+
+//$globalist['list'] = $globalitems;
+
    $globalitems =  array (
+    //'ATTRIBUTE' => $matchList[2],
     'service' => "iptv",
     'item' => $items,
     );
