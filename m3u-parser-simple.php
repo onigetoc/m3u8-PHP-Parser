51aa0220bb1b96f80bfcbc71398c9971ba630fbf
Gino <ginocote@gmail.com>
2017-10-18 00:27:37 -0400

    Simpler

--- m3u-parser-simple.php
+++ m3u-parser-simple.php
@@ -1,66 +1,30 @@
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('allow-origin: *');
-
-header("Access-Control-Allow-Headers: ACCEPT, CONTENT-TYPE, X-CSRF-TOKEN");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
-//header('Access-Control-Allow-Origin: http://www.foo.com', false);
-
-//    if (isset($_SERVER['HTTP_ORIGIN'])) {
-//        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-//        header('Access-Control-Allow-Credentials: true');
-//        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-//    }
-//
-//    // Access-Control headers are received during OPTIONS requests
-//    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-//
-//        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-//            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
-//
-//        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-//            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-//
-//        exit(0);
-//    }
 
 $url = $_GET["url"];
 
 if(isset($url)) {
   $m3ufile = file_get_contents($url);
 } else {
-  $m3ufile = file_get_contents('https://pastebin.com/AJGb4D3Y');
+  $m3ufile = file_get_contents('http://pastebin.com/raw/t1mBJ2Yi');
 }
+
+//$m3ufile = str_replace('tvg-', 'tvg_', $m3ufile);
+$m3ufile = str_replace('group-title', 'tvgroup', $m3ufile);
+$m3ufile = str_replace("tvg-", "tv", $m3ufile);
 
 //$re = '/#(EXTINF|EXTM3U):(.+?)[,]\s?(.+?)[\r\n]+?((?:https?|rtmp):\/\/(?:\S*?\.\S*?)(?:[\s)\[\]{};"\'<]|\.\s|$))/';
 $re = '/#EXTINF:(.+?)[,]\s?(.+?)[\r\n]+?((?:https?|rtmp):\/\/(?:\S*?\.\S*?)(?:[\s)\[\]{};"\'<]|\.\s|$))/';
-//$attributes = '/([a-zA-Z0-9\-]+?)="([^"]*)"/';
-$attributes = '/([a-zA-Z0-9\-\_]+?)="([^"]*)"/';
-
-
-//$m3ufile = str_replace('tvg-logo', 'thumb_square', $m3ufile);
-//$m3ufile = str_replace('tvg-id', 'id', $m3ufile);
-////$m3ufile = str_replace('tvg-name', 'group', $m3ufile);
-////$m3ufile = str_replace('tvg-name', 'name', $m3ufile);
-//$m3ufile = str_replace('tvg-name', 'author', $m3ufile);
-//$m3ufile = str_replace('group-title', 'group', $m3ufile);
-
-// WAY TO REPLACE ALL -
-//$m3ufile = str_replace('-', '', $m3ufile);
-
-//print_r($m3ufile);
-
-//$m3ufile = str_replace(' ', '_', $m3ufile); // FOR GROUP
+$attributes = '/([a-zA-Z0-9\-]+?)="([^"]*)"/';
 
 preg_match_all($re, $m3ufile, $matches);
 
 // Print the entire match result
 //print_r($matches);
 
-$items = array();
-
 $i = 1;
+
+$items = array();
 
  foreach($matches[0] as $list) {
     
@@ -79,14 +43,8 @@ $i = 1;
    $newdata =  array (
     //'ATTRIBUTE' => $matchList[2],
     'id' => $i++,
-    'service' => "iptv",
-    'title' => $matchList[2],
-    //'playlistURL' => (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",
-    'playlistURL' => $_SERVER['QUERY_STRING'],
-    'playlistURL' => str_replace("url=","",$_SERVER['QUERY_STRING']),
-    'media_url' => $mediaURL,
-//    'author' => "Author name or provider",
-    'url' => $mediaURL
+    'tvtitle' => $matchList[2],
+    'tvmedia' => $mediaURL
     );
     
     preg_match_all($attributes, $list, $matches, PREG_SET_ORDER);
@@ -103,31 +61,13 @@ $i = 1;
     
  }
 
-//   $globalitem =  array (
-//    //'ATTRIBUTE' => $matchList[2],
-//    'item' => $items
-//    );
-
-//$globalitem[$items] ;
-//$globalitems['item'] = $items;
-
-//$globalist['list'] = $globalitems;
-
-   $globalitems =  array (
-    //'ATTRIBUTE' => $matchList[2],
-    'service' => "iptv",
-    'item' => $items,
-    );
-
-  $globalist['list'] = $globalitems;
-
 //print_r($items);
 
 $callback= $_GET['callback'];
 
   if($callback)
-    echo $callback. '(' . json_encode($globalist) . ')';  // jsonP callback
+    echo $callback. '(' . json_encode($items) . ')';  // jsonP callback
   else
-    echo json_encode($globalist);
+    echo json_encode($items);
 
 ?>
