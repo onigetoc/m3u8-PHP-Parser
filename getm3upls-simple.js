82ffed9d66f40c50b7073b8e8b664b93a4bbfd4e
Gino <ginocote@gmail.com>
2017-11-15 15:53:26 -0500

    jQuery demo for m3u-parser-simple.php

---
+++ getm3upls-simple.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    getPlaylists("https://pastebin.com/raw/t1mBJ2Yi");
+    $('#m3uForm').on('submit', function(e) {
+        e.preventDefault();
+        var playlists = $('#playlists').val();
+        getPlaylists(playlists);
+    });
+});
+function getPlaylists(playlists) {
+    $('#result').html('');
+    $.ajax({
+        url: '//yourwebsite.com/m3u8-PHP-Parser/m3u-parser-simple.php',
+        method: 'GET',
+        dataType: 'jsonP',
+        data: {
+            url: playlists,
+        }
+    }).done(function(data) {
+        $('#result').text('Total: ' + data.length + ' Channels found');
+        $.each(data, function(i, item) {
+            var tvglogo = '';
+            if (typeof item.tvlogo != 'undefined') tvglogo = '<img src="' + item.tvlogo + '" alt="' + item.tvtitle + '" style="width:38px;height:38px;float:left;marging: 5px;border:solid 1px #ccc;margin-right:5px">';
+            $('#result').append('<li><a href="' + item.tvmedia + '">' + tvglogo + ' â–º ' + item.tvtitle + '</a><br>' + item.tvmedia + '</li>');
+        });
+    });
+}
+$(document).on('click', '#result a', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var vidTitle = $this.text().replace("â–º ", "");
+    $('#vidTitle').text(vidTitle);
+    $('#result a').removeClass('bold');
+    $this.addClass('bold');
+    var mediaUrl = $this.attr('href');
+    loadStream(mediaUrl)
+});
+$(document).on('click', '#menu a', function(e) {
+    e.preventDefault();
+    $('#menu a').removeClass('active');
+    $(this).addClass('active');
+    var mediahref = $(this).attr('href');
+    $('#playlists').val(mediahref);
+    $("#envoyer").trigger("click");
+});
